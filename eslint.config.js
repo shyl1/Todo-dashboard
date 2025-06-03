7b6281adbbdbfb28c6749333936708e83f705178
shyl <sohaylae615@gmail.com>
2025-06-03 23:50:50 +0300

    fix: update firebase config and ignore .env

--- eslint.config.js
+++ eslint.config.js
@@ -10,10 +10,7 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        process: 'readonly', // Add process to globals
-      },
+      globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -25,14 +22,12 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      node: nodePlugin, // Add node plugin
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'node/no-process-env': 'off', // Disable if you want to allow process.env
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
